65b517f202d2353d2447f21af8b4c8dcca70f0a4
mailhyuil <mailhyuil@gmail.com>
2023-04-30 15:14:43 +0900

    chore:add post

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { ModalComponent } from "./modal/modal.component";
-import { ButtonComponent } from "./button/button.component";
-import {HttpClient, HttpClientModule} from "@angular/common/http"
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ModalComponent } from './modal/modal.component';
+import { ButtonComponent } from './button/button.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   standalone: true,
-  imports: [ RouterModule, ModalComponent, ButtonComponent, HttpClientModule],
-  selector: "nx-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  imports: [RouterModule, ModalComponent, ButtonComponent, HttpClientModule],
+  selector: 'nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "client";
-  data:any
-  constructor(private readonly http:HttpClient){}
-   submit(){
-    const observable = this.http.get('http://localhost:3000/api')
-     observable.subscribe((data)=>{console.log(data);this.data=data})
-    console.log('hi')
+  title = 'client';
+  data: any;
+  constructor(private readonly http: HttpClient) {}
+  submit() {
+    const observable = this.http.get('http://localhost:8080/api');
+    observable.subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
+    console.log('hi');
   }
 }
--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -5,9 +5,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
-  getData() {
-    return this.appService.getData();
+  hi() {
+    return { message: 'hi' };
   }
 }
